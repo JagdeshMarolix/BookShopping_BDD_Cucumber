895676dc9bc20125e239a7657f791f987567d657
unknown <jmarolix@gmail.com>
2023-12-15 07:24:28 +0530

    junit parallel execution without parallel execution

---
+++ src/test/resource/AppFeatures/acc.feature
@@ -0,0 +1,24 @@
+Feature: Dashboard Page Feature
+
+Background:
+Given user has already logged in to application
+|username|password|
+|jmarolix@gmail.com|Jagdeesh@1|
+
+@accounts
+Scenario: Accounts page title
+Given user is on Accounts page
+When user gets the title of the page
+Then page title should be "My Account – Automation Practice Site"
+
+@acccounts
+Scenario: Accounts section count			
+Given user is on Accounts page
+Then user gets accounts section
+|Dashboard|
+|Orders|	
+|Downloads|
+|Addresses|
+|Account Details|
+|Logout|
+And accounts section count should be 6
--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,58 +1,80 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "c0accf78-a83c-4f2f-84a2-b3559070e08c",
+    "id": "86d541d6-eb95-4de7-81f0-4eafce0aaefb",
     "feature": "Dashboard Page Feature",
     "scenario": "Accounts page title",
-    "start": 1702538133595,
+    "start": 1702604573402,
     "group": 1,
     "content": "",
     "tags": "@accounts,",
-    "end": 1702538145758,
+    "end": 1702604586892,
     "className": "passed"
   },
   {
-    "id": "f9cfffa8-b014-454f-b4c8-300f8a496747",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1702538156632,
+    "id": "2497721c-515d-4fdb-8400-b87d9fc1ef90",
+    "feature": "Dashboard Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1702604627195,
     "group": 1,
     "content": "",
-    "tags": "",
-    "end": 1702538164849,
+    "tags": "@accounts,",
+    "end": 1702604639782,
     "className": "passed"
   },
   {
-    "id": "31e701fd-9126-4628-9ea4-02e41dafdb87",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1702538174926,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1702538185501,
-    "className": "passed"
-  },
-  {
-    "id": "4fd06518-f834-4390-a336-dcd165aaf881",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1702538164859,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1702538174906,
-    "className": "failed"
-  },
-  {
-    "id": "fb1a21b0-db09-4339-8ecc-92b6f1f5521d",
+    "id": "a04558c1-8cc7-4e78-a07b-85e87d3b51c0",
     "feature": "Dashboard Page Feature",
     "scenario": "Accounts section count",
-    "start": 1702538145788,
+    "start": 1702604586905,
     "group": 1,
     "content": "",
     "tags": "@acccounts,",
-    "end": 1702538156619,
+    "end": 1702604599250,
+    "className": "passed"
+  },
+  {
+    "id": "654bf1e9-ab31-41bc-96e6-ffa9fd77b2dd",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1702604599261,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1702604608599,
+    "className": "passed"
+  },
+  {
+    "id": "d4ac135a-55de-411a-828b-0b1abdcde5d1",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1702604608609,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1702604617885,
+    "className": "failed"
+  },
+  {
+    "id": "33f007e2-b93b-43c6-be50-0bd25032334f",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1702604617899,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1702604627187,
+    "className": "passed"
+  },
+  {
+    "id": "9f0f1c59-6bff-4526-ab36-e78b38a1bc3a",
+    "feature": "Dashboard Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1702604639788,
+    "group": 1,
+    "content": "",
+    "tags": "@acccounts,",
+    "end": 1702604652909,
     "className": "passed"
   }
 ]);
